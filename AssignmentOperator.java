a88b9aa3d11906ceb67c5924a93dab99f206bb8b
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-03-08 00:38:36 +0530

    Create AssignmentOperator.java

---
+++ AssignmentOperator.java
@@ -0,0 +1,16 @@
+class assignmentoperator {
+    public static void main(String[] args) {
+        int a = 4;
+        int var;
+        var = a;
+        System.out.println("var using  = :"+var);
+        var += var;
+        System.out.println("Multiplication and assignment += :"+var);
+        var /= var;
+        System.out.println(" Division and assignment :"+var);
+        var -= var;
+        System.out.println("Subtraction and assignment *= :"+var);
+        
+        
+    }
+}
